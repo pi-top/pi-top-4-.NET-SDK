5a725cec14fd1e5498e77d65209845ec9c4647f2
Diego <dicolomb@microsoft.com>
2020-04-03 12:15:00 +0100

    update svg

--- src/PiTopMakerArchitecture.Foundation.InteractiveExtension/DeviceExtensions.cs
+++ src/PiTopMakerArchitecture.Foundation.InteractiveExtension/DeviceExtensions.cs
@@ -11,10 +11,10 @@ namespace PiTopMakerArchitecture.Foundation.InteractiveExtension
         {
             var id = "PiTopMakerArchitecture.Foundation.InteractiveExtension" + Guid.NewGuid().ToString("N");
             return PocketViewTags.div[id: id](
-                PocketViewTags.svg[viewBox: "0 0 40 60"](
+                PocketViewTags.svg(
                     PocketViewTags.g(
-                        PocketViewTags.circle[cx: 20, cy: 20, r: 15, fill:(led.IsOn? "black":"white"), style: "stroke: black, stroke-width:2"],
-                        PocketViewTags.text[x: 0, y: 0, @class: "text"]("LED")
+                        PocketViewTags.circle[cx: 20, cy: 20, r: 15, fill:(led.IsOn? "white":"black"), stroke: "black"],
+                        PocketViewTags.text[x: 9, y: 50, @class: "text"]("LED")
                     )));
         }
     }
