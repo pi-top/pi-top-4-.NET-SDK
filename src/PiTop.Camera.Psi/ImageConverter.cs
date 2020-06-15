4055869ecf6aba41c4751f1805724a1c79bcdc15
Diego <dicolomb@microsoft.com>
2020-06-15 12:27:42 +0100

    first stab at Psi integration

---
+++ src/PiTop.Camera.Psi/ImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+using Microsoft.Psi;
+
+using OpenCvSharp;
+
+namespace PiTop.Camera.Psi
+{
+    public static class ImageConverter
+    {
+        public static IProducer<Microsoft.Psi.Imaging.Image> ToImage(this IProducer<Bitmap> bitmapStream, Pipeline pipeline)
+        {
+            return (new BitmapImageStreamWrapper(bitmapStream, pipeline)).Select(e => e.Resource);
+        }
+
+        public static IProducer<Microsoft.Psi.Imaging.Image> ToImage(this IProducer<Mat> matStream, Pipeline pipeline)
+        {
+            return (new MatImageStreamWrapper(matStream, pipeline)).Select(e => e.Resource);
+        }
+    }
+}
