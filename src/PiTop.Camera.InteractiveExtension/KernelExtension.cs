f4434215904c9c54516e5e09fe400f5e7d901655
Diego <dicolomb@microsoft.com>
2020-06-15 16:06:05 +0100

    add packaging

---
+++ src/PiTop.Camera.InteractiveExtension/KernelExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.DotNet.Interactive;
+
+namespace PiTop.Camera.InteractiveExtension
+{
+    public class KernelExtension : IKernelExtension
+    {
+        public Task OnLoadAsync(IKernel kernel)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
