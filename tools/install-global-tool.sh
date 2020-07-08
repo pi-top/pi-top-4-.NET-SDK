7626c7d850fa84b5fa9b3f21a7fd7cb5f5a0f760
Diego <dicolomb@microsoft.com>
2020-07-08 18:38:06 +0100

    install and update

---
+++ tools/install-global-tool.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet tool install -g --add-source "https://dotnet.myget.org/F/dotnet-try/api/v3/index.json" Microsoft.dotnet-interactive
---
+++ tools/update-global-tool.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet tool update -g --add-source "https://dotnet.myget.org/F/dotnet-try/api/v3/index.json" Microsoft.dotnet-interactive
