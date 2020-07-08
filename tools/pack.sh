cd9ef9f37db92c0b3b9f37149e62fbd9750e335e
Diego <dicolomb@microsoft.com>
2020-07-08 22:53:45 +0100

    better pack sh

--- tools/pack.sh
+++ tools/pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-dotnet pack ../src/PiTopMakerArchitecture.Foundation.Psi.nuget/PiTopMakerArchitecture.Foundation.Psi.nuget.csproj /p:PackageVersion=$1 -o /home/pi/localNuget
-dotnet pack ../src/PiTopMakerArchitecture.Foundation.nuget/PiTopMakerArchitecture.Foundation.nuget.csproj /p:PackageVersion=$1 -o /home/pi/localNuget
-dotnet pack ../src/PiTop.Camera.Psi.nuget/PiTop.Camera.Psi.nuget.csproj /p:PackageVersion=$1 -o /home/pi/localNuget
-dotnet pack ../src/PiTop.Camera.nuget/PiTop.Camera.nuget.csproj /p:PackageVersion=$1 -o /home/pi/localNuget
-dotnet pack ../src/PiTop.nuget/PiTop.nuget.csproj /p:PackageVersion=$1 -o /home/pi/localNuget
+projects=$(find ../src/ -path "*.nuget.csproj" ) 
+for project in $projects
+do
+ echo "poacking " $project
+ dotnet pack $project /p:PackageVersion=$1 -o /home/pi/localNuget
+done
