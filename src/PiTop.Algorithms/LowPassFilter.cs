ddc8ed59ea0fe107d1463fb8bc5d5e4b46cf5ae2
Diego Colombo <dicolomb@microsoft.com>
2020-10-10 10:37:25 +0100

    add filters

---
+++ src/PiTop.Algorithms/LowPassFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PiTop.Algorithms
+{
+    public class LowPassFilter : IFilter
+    {
+        private readonly double _alpha;
+        private double _previousValue;
+        public LowPassFilter(double initialValue = double.NaN, double alpha = 0.5)
+        {
+            _alpha = alpha;
+            _previousValue = initialValue;
+        }
+
+        public double Apply(double value, DateTime? now = null)
+        {
+            if (double.IsNaN(_previousValue))
+            {
+                _previousValue = value;
+                return value;
+            }
+
+            var previousValue = _previousValue;
+            _previousValue = _alpha * value + (1 - _alpha) * previousValue;
+
+            return _previousValue;
+        }
+    }
+}
---
+++ src/PiTop.Algorithms/OneEuroFilter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace PiTop.Algorithms
+{
+    /// <summary>
+    /// Smoothing filter adapted from https://github.com/jaantollander/OneEuroFilter
+    /// </summary>
+    public class OneEuroFilter
+    {
+        private DateTime _previousTime;
+        private double _previousValue;
+        private double _previousDelta;
+        private readonly double _minCutoff;
+        private readonly double _beta;
+        private readonly double _cutoff;
+
+        public OneEuroFilter(DateTime? currentTime = null, double initialValue = double.NaN, double delta = 0.0, double minCutoff = 1.0, double beta = 0.0,
+            double cutoff = 1.0)
+        {
+            _previousTime = currentTime ?? DateTime.UtcNow;
+            _previousValue = initialValue;
+            _previousDelta = delta;
+            _minCutoff = minCutoff;
+            _beta = beta;
+            _cutoff = cutoff;
+        }
+
+        public double Apply(double value, DateTime? now = null)
+        {
+            now = now?.ToUniversalTime() ?? DateTime.UtcNow;
+
+            if (double.IsNaN(_previousValue))
+            {
+                _previousValue = value;
+                _previousTime = now.Value;
+                return value;
+            }
+
+            var elapsedMilliseconds = (now.Value - _previousTime).TotalMilliseconds;
+
+            // The filtered derivative of the signal.
+            var alpha = SmoothingFactor(elapsedMilliseconds, _cutoff);
+            var dx = (value - _previousValue) / elapsedMilliseconds;
+            var dxHat = ExponentialSmoothing(alpha, dx, _previousDelta);
+
+            // The filtered signal.
+            var cutoff = _minCutoff + _beta * Math.Abs(dxHat);
+            alpha = SmoothingFactor(elapsedMilliseconds, cutoff);
+            var xHat = ExponentialSmoothing(alpha, value, _previousValue);
+
+            // Memorize the previous values.
+            _previousValue = xHat;
+            _previousDelta = dxHat;
+            _previousTime = now.Value;
+
+            return xHat;
+        }
+
+        private static double SmoothingFactor(double elapsedMilliseconds, double cutoff)
+        {
+            var r = 2 * Math.PI * cutoff * elapsedMilliseconds;
+            return r / (r + 1);
+        }
+
+
+        private static double ExponentialSmoothing(double alpha, double value, double previousValue)
+        {
+            return alpha * value + (1 - alpha) * previousValue;
+        }
+    }
+}
