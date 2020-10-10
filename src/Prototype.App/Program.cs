f43f9c1af2fad4342294a8845c37d3c19a068745
Paul Meyer <paulmey@microsoft.com>
2020-10-09 23:14:03 -0700

    Study ultrasonic sensor peformance

--- src/Prototype.App/Program.cs
+++ src/Prototype.App/Program.cs
@@ -2,18 +2,16 @@
 using PiTop.Camera;
 using PiTop.MakerArchitecture.Expansion;
 using PiTop.MakerArchitecture.Expansion.Rover;
-using PiTop.MakerArchitecture.Foundation;
-
 using Pocket;
 
 using SixLabors.ImageSharp;
 
 using System;
+using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 
 using UnitsNet;
-
-using static Pocket.Logger;
 
 namespace Prototype.App
 {
@@ -23,11 +21,12 @@ namespace Prototype.App
         {
             LogEvents.Subscribe(i => Console.WriteLine(i.ToLogString()), new[]
             {
-                typeof(PiTop4Board).Assembly,
-                typeof(FoundationPlate).Assembly,
-                typeof(ExpansionPlate).Assembly,
-                typeof(RoverRobot).Assembly,
-                typeof(StreamingCamera).Assembly,
+                //typeof(PiTop4Board).Assembly,
+                //typeof(FoundationPlate).Assembly,
+                //typeof(ExpansionPlate).Assembly,
+                //typeof(RoverRobot).Assembly,
+                //typeof(StreamingCamera).Assembly,
+                //typeof(UltrasonicSensor).Assembly,
                 typeof(Program).Assembly,
             });
 
@@ -36,7 +35,7 @@ namespace Prototype.App
             // using ` mjpg_streamer -i "input_uvc.so -d /dev/video0" -o output_http.so`
             // ==> http://pi-top.local:8080/?action=stream
             PiTop4Board.Instance.UseCamera();
-            var rover = new RoverRobot(PiTop4Board.Instance.GetOrCreateExpansionPlate(), PiTop4Board.Instance.GetOrCreateCamera<StreamingCamera>(0),
+            using var rover = new RoverRobot(PiTop4Board.Instance.GetOrCreateExpansionPlate(), PiTop4Board.Instance.GetOrCreateCamera<StreamingCamera>(0),
                 RoverRobotConfiguration.Default);
             var camControl = rover.TiltController;
             var motorControl = rover.MotionComponent as SteeringMotorController;
@@ -49,7 +48,9 @@ namespace Prototype.App
                 .DistinctUntilChanged()
                 .Subscribe(stick =>
                 {
-                    motorControl.SetPower((stick.Y + stick.X) / 2, (stick.Y - stick.X) / 2);
+                    var left = stick.X.WithDeadzone(-.5, .5, .3);
+                    var forward = stick.Y;
+                    motorControl.SetPower((forward + left) / 1.5, (forward - left) / 1.5);
 
                 });
 
@@ -69,16 +70,7 @@ namespace Prototype.App
             js.Events.OfType<ButtonEvent>().Where(e => e.Button == Button.X && e.Pressed)
                 .Subscribe(e =>
                 {
-                    using var operation = Log.OnEnterAndConfirmOnExit();
-                    try
-                    {
-                        rover.Camera.GetFrame().Save("/home/pi/shot.jpg");
-                        operation.Succeed();
-                    }
-                    catch (Exception ex)
-                    {
-                        operation.Error(ex);
-                    }
+                    rover.Camera.GetFrame().Save("/home/pi/shot.jpg");
                 });
 
             Observable.Interval(TimeSpan.FromMilliseconds(100))
@@ -93,10 +85,26 @@ namespace Prototype.App
 
                 });
 
+            js.Events.OfType<ButtonEvent>().Subscribe(e => Console.WriteLine($"Button: {e.Button} {e.Pressed}"));
             js.Events.OfType<ButtonEvent>().Where(e => e.Button == Button.RightStick)
                 .Subscribe(e =>
                 {
                     camControl.Reset();
+                });
+
+            Observable.Interval(TimeSpan.FromMilliseconds(100))
+                .Select(_ =>
+                new Unit[10].Select(_ => rover.UltrasoundFront.Distance.Centimeters).ToArray())
+                .Subscribe(l =>
+                {
+                    var mean = l.Average();
+                    var stddev = Math.Sqrt(l.Select(d => (d - mean) * (d - mean)).Average());
+                    var maxrange = 1.5 * stddev;
+                    var valid = l.Where(d => Math.Abs(d - mean) < maxrange).ToList();
+                    if (valid.Count > 0)
+                    {
+                        Console.WriteLine($"Distance= {valid.Average():F1} cm ({valid.Count}: mean {mean:F1}, stddev {stddev:F1}, max {valid.Max():F1}, min {valid.Min():F1})");
+                    }
                 });
 
             Console.WriteLine("Ok, go drive around");
