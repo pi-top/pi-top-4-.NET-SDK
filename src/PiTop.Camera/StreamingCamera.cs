70b7be17528c11338ece586e7f5e5eae1f4b325f
Paul Meyer <paulmey@microsoft.com>
2020-10-11 00:37:25 -0700

    Split camera so that it can rely on system-managed mjpg_streamer

--- src/PiTop.Camera/StreamingCamera.cs
+++ src/PiTop.Camera/StreamingCamera.cs
@@ -1,11 +1,4 @@
-﻿using Pocket;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using System;
-using System.Diagnostics;
-using System.Net.Http;
-
-using static Pocket.Logger;
+﻿using System.Diagnostics;
 
 namespace PiTop.Camera
 {
@@ -13,43 +6,27 @@ namespace PiTop.Camera
     /// Follow instructions from https://github.com/jacksonliam/mjpg-streamer to compile and install mjpg-streamer.
     /// When the camera is connected, the stream is also viewable externally on port 8080 (barring any firewalls).
     /// </summary>
-    public class StreamingCamera :
-         ICamera,
-         IFrameSource<Image<Rgb24>>,
-        IDisposable
+    public class StreamingCamera : HttpSnapshotCamera
     {
         private Process? _process;
-        private HttpClient _client;
         private readonly int _cameraId;
 
-        public StreamingCamera(int cameraId)
+        public StreamingCamera(int cameraId) : base(cameraId)
         {
             _cameraId = cameraId;
         }
 
-        public void Connect()
+        public override void Connect()
         {
-            _client = new HttpClient();
             _process = Process.Start(new ProcessStartInfo("mjpg_streamer",
                 $"-i \"input_uvc.so -d /dev/video{_cameraId}\" -o output_http.so"));
+            base.Connect();
         }
-        public void Dispose()
+
+        public override void Dispose()
         {
+            base.Dispose();
             _process?.Dispose();
-            _client?.Dispose();
-        }
-
-        public Image GetFrame()
-        {
-            using var operation = Log.OnEnterAndExit();
-            var image = _client.GetByteArrayAsync("http://localhost:8080/?action=snapshot").Result;
-            using var _ = operation.OnEnterAndExit("Image.Load");
-            return Image.Load(image);
-        }
-
-        Image<Rgb24> IFrameSource<Image<Rgb24>>.GetFrame()
-        {
-            return GetFrame().CloneAs<Rgb24>();
         }
     }
 }
