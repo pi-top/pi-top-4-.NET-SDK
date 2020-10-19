b21406ffcfa9bb82bea3e1073fabf04d9b165d9e
Diego <dicolomb@microsoft.com>
2020-10-19 22:17:38 +0100

    added test cases

--- src/PiTop.Algorithms.Tests/VelocitySensorTests.cs
+++ src/PiTop.Algorithms.Tests/VelocitySensorTests.cs
@@ -32,5 +32,51 @@ namespace PiTop.Algorithms.Tests
             scheduler.AdvanceBy(TimeSpan.FromSeconds(3).Ticks);
             sensor.Velocity.Should().Be(Speed.FromMetersPerSecond(4));
         }
+
+        [Fact]
+        public void single_sample_calculates_zero()
+        {
+            var accelerations = new[]
+            {
+                Acceleration.FromMetersPerSecondSquared(2),
+                Acceleration.FromMetersPerSecondSquared(2),
+                Acceleration.FromMetersPerSecondSquared(0)
+            };
+
+            var pos = 0;
+            var scheduler = new TestScheduler();
+            using var sensor = new VelocitySensor(TimeSpan.FromSeconds(1), () =>
+            {
+                var index = pos;
+                pos++;
+                return index < accelerations.Length ? accelerations[index] : Acceleration.Zero;
+            }, scheduler);
+
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(1.1).Ticks);
+            sensor.Velocity.Should().Be(Speed.Zero);
+        }
+
+        [Fact]
+        public void negative_accelerations_decrease_velocity()
+        {
+            var accelerations = new[]
+            {
+                Acceleration.FromMetersPerSecondSquared(2),
+                Acceleration.FromMetersPerSecondSquared(-2),
+                Acceleration.FromMetersPerSecondSquared(0)
+            };
+
+            var pos = 0;
+            var scheduler = new TestScheduler();
+            using var sensor = new VelocitySensor(TimeSpan.FromSeconds(1), () =>
+            {
+                var index = pos;
+                pos++;
+                return index < accelerations.Length ? accelerations[index] : Acceleration.Zero;
+            }, scheduler);
+
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(3).Ticks);
+            sensor.Velocity.Should().Be(Speed.Zero);
+        }
     }
 }
