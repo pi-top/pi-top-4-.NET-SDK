3a170d9d0689983c2201ad63f7441c1a9ae55bd1
Diego <dicolomb@microsoft.com>
2020-10-20 21:36:14 +0100

    addressing feedback

--- src/PiTop.Algorithms/VelocitySensor.cs
+++ src/PiTop.Algorithms/VelocitySensor.cs
@@ -1,30 +1,38 @@
 ﻿using System;
-using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+
 using UnitsNet;
 
 namespace PiTop.Algorithms
 {
+    /// <summary>
+    /// Calculates the velocity by sampling acceleration values.
+    /// The initial velocity of the sensor is 0 m/s and it will require a pair of sample for first value .
+    /// </summary>
     public class VelocitySensor : IDisposable
     {
         private readonly IDisposable _sampler;
         private (DateTimeOffset Timestamp, Acceleration Accelecation)? _previousSample;
-        private IScheduler _scheduler;
 
         public VelocitySensor(TimeSpan samplingFrequency, Func<Acceleration> accelerationSampler, IScheduler samplingScheduler = null)
         {
             Velocity = Speed.Zero;
-            _scheduler = samplingScheduler ?? TaskPoolScheduler.Default;
-            var samplingFrequency1 = samplingFrequency;
-            if (samplingFrequency1.TotalMilliseconds <= 0)
+            var scheduler = samplingScheduler ?? TaskPoolScheduler.Default;
+            
+            if (samplingFrequency.TotalMilliseconds <= 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(samplingFrequency), "Sampling frequency must be greater than 0 ms");
             }
-            var accelerationSampler1 = accelerationSampler ?? throw new ArgumentNullException(nameof(accelerationSampler));
+
+            accelerationSampler = accelerationSampler ?? throw new ArgumentNullException(nameof(accelerationSampler));
             _sampler = Observable
-                .Interval(samplingFrequency1, _scheduler)
-                .Select(_ => (_scheduler.Now, accelerationSampler1()))
+                .Interval(samplingFrequency, scheduler)
+                .Select(_ =>
+                {
+                    var acceleration = accelerationSampler();
+                    return (scheduler.Now, acceleration);
+                })
                 .Subscribe(OnNext);
         }
 
@@ -42,7 +50,9 @@ namespace PiTop.Algorithms
             _previousSample = newSample;
         }
 
-
+        /// <summary>
+        /// The velocity
+        /// </summary>
         public Speed Velocity { get; private set; }
 
 
