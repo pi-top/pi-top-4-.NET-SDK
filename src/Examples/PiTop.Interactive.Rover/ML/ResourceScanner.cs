1a722bf7ea92b1736794674dbbaa034f208ad311
Diego <dicolomb@microsoft.com>
2020-10-30 01:35:04 +0000

    enable the use of lobe endpoint

--- src/Examples/PiTop.Interactive.Rover/ML/ResourceScanner.cs
+++ src/Examples/PiTop.Interactive.Rover/ML/ResourceScanner.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 
 using lobe;
 using lobe.ImageSharp;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json.Linq;
 
 using PiTop.Camera;
 
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace PiTop.Interactive.Rover.ML
@@ -14,6 +21,8 @@ namespace PiTop.Interactive.Rover.ML
     public class ResourceScanner
     {
         private ImageClassifier _classifier;
+        private Uri _predictionEndpoint;
+        private HttpClient _client;
         public Func<Image> CaptureImage { get; set; }
 
         public double Threshold { get; set; }
@@ -24,11 +33,20 @@ namespace PiTop.Interactive.Rover.ML
             Threshold = 0.9;
         }
 
-        public void LoadModel(DirectoryInfo modelLocation)
+        public void UseModel(DirectoryInfo modelLocation)
         {
+            _predictionEndpoint = null;
             _classifier?.Dispose();
             _classifier = ImageClassifier.CreateFromSignatureFile(
                  new FileInfo(Path.Combine(modelLocation.FullName, "signature.json")), format: "onnx", modelFileName: "saved_model.onnx");
+        }
+
+        public void UseUri(Uri predictionEndpoint)
+        {
+            _classifier?.Dispose();
+            _classifier = null;
+            _predictionEndpoint = predictionEndpoint;
+            _client = new HttpClient();
         }
 
         public ClassificationResults Scan()
@@ -39,8 +57,54 @@ namespace PiTop.Interactive.Rover.ML
 
         public ClassificationResults AnalyseFrame(Image frame)
         {
+            ClassificationResults results = null;
+            if (_classifier != null)
+            {
+                results = UseClassifier(frame);
+            }
+            else if (_predictionEndpoint != null)
+            {
+                results = UseService(frame);
+            }
+
+            return results;
+
+        }
+
+        private ClassificationResults UseService(Image frame)
+        {
+            var image = frame.CloneAs<Rgb24>();
+            using var stream = new MemoryStream();
+            image.Save(stream, new PngEncoder());
+            stream.Flush();
+            var data = stream.ToArray();
+            var imageSource = $"{Convert.ToBase64String(data)}";
+
+            var content = new StringContent($"{{ \"inputs\": {{ \"Image\":  \"{imageSource}\" }} }}", Encoding.UTF8,
+                "application/json");
+            var response = _client.PostAsync(_predictionEndpoint, content).Result;
+            var body = response.Content.ReadAsStringAsync().Result;
+
+            var classification = JObject.Parse(body);
+
+            var classifications = classification.SelectToken("outputs.Labels").Values<JArray>()
+                .Select(ja => new Classification(ja[0].Value<string>(), ja[1].Value<double>())).ToArray();
+
+
+            var classificationResults =
+                new ClassificationResults(
+                    classifications.First(c => c.Label == classification.SelectToken("outputs.Prediction[0]").Value<string>()),
+                    classifications);
+
+            var results = classificationResults?.Classification?.Confidence > Threshold ? classificationResults : null;
+            return results;
+        }
+
+        private ClassificationResults UseClassifier(Image frame)
+        {
             var classificationResults = frame != null ? _classifier?.Classify(frame.CloneAs<Rgb24>()) : null;
-            return classificationResults?.Classification?.Confidence > Threshold ? classificationResults : null;
+            var results = classificationResults?.Classification?.Confidence > Threshold ? classificationResults : null;
+            return results;
         }
     }
 }
