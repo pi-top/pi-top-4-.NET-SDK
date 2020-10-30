562f804dcbbf16a282ddc6971e88a6463a1eb6d1
Diego <dicolomb@microsoft.com>
2020-10-30 02:43:43 +0000

    add focus to crop in the centre of the camera

--- src/Examples/PiTop.Interactive.Rover/ImageProcessing/Extensions.cs
+++ src/Examples/PiTop.Interactive.Rover/ImageProcessing/Extensions.cs
@@ -18,6 +18,12 @@ namespace PiTop.Interactive.Rover.ImageProcessing
             return source.CloneAndResize(width, height);
         }
 
+        public static Image Focus(this Image source)
+        {
+            var rect = new Rectangle(source.Width / 2, source.Height / 4, source.Width / 2, source.Height / 2);
+            return source.Clone(c => c.Crop(rect));
+        }
+
         public static Image<TPixel> CloneAndResize<TPixel>(this Image<TPixel> source, int width, int height) where TPixel : unmanaged, IPixel<TPixel>
         {
             return source.Clone(c => c.Resize(width, height));
@@ -28,6 +34,12 @@ namespace PiTop.Interactive.Rover.ImageProcessing
             var width = 600;
             var height = (int)(((double)width / source.Width) * source.Height);
             return source.CloneAndResize(width, height);
+        }
+
+        public static Image<TPixel> Focus<TPixel>(this Image<TPixel> source) where TPixel : unmanaged, IPixel<TPixel>
+        {
+            var rect = new Rectangle(source.Width/4, source.Height/4, source.Width/2, source.Height/2);
+            return source.Clone(c => c.Crop(rect));
         }
     }
 }
