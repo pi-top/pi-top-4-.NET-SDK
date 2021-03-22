27ca957c2b78c4abe497a79b85741b0ffd58c65f
Diego Colombo <dicolomb@microsoft.com>
2021-03-22 11:19:42 +0000

    add license info to nuget packages (#58)

--- tools/pack.sh
+++ tools/pack.sh
@@ -13,7 +13,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 root="$(dirname "${DIR}")"
 
 # Default to using version 1.1.1 and installing in /home/pi/localNuget
-version="${1:-1.1.1}"
+version="${1:-0.0.1-local}"
 localNugetPath="${2:-/home/pi/localNuget}"
 
 dotnet build "${root}/src" -c Release
