f659efc41588803d10be5ad597a874b8fa1a2b3e
colombod <colombod@me.com>
2021-08-03 11:08:55 +0100

    fixes

--- src/PiTop/PlatePortInUseException.cs
+++ src/PiTop/PlatePortInUseException.cs
@@ -3,7 +3,11 @@
 namespace PiTop
 {
     public sealed class  PlatePortInUseException : Exception{
-        public PlatePortInUseException(PlatePort port): base($"Port already in use by (another) {port.Device}")
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="port">The Port that is already used.</param>
+        public PlatePortInUseException(PlatePort port): base($"Port {port.Name} already in use by (another) {port.Device}")
         {
             
         }
